c8dd0cc84a0ab11de77239ae42aef3e7ef54bdce
da82bc6f4bd07a12b24f59a324dcc5b65896a53e <hohle.paul@gmail.com>
2020-05-12 09:54:42 -0700

    Add controller packet examples using struct and MsgPack

--- msgpack-and-struct/python/ex3.py
+++ msgpack-and-struct/python/ex3.py
@@ -9,9 +9,19 @@ MSG_UPDATE_PID = 0x2000
 
 def msgpack_ex():
 
+  header       = MSG_UPDATE_PID
+
   proportional = 1.0
   integral     = 2.0
   derivitive   = 3.0
+  gain         = 4.0
+
+  packet = (header,proportional,integral,derivitive,gain)
+  tx     = msgpack.packb(packet)
+  rx     = msgpack.unpackb(tx)
+
+  print("MsgPack Packed TX packet   : ",tx)
+  print("MsgPack Unpacked RX packet : ",rx)
 
 #***************************************************************************************************************
 
@@ -19,20 +29,33 @@ def struct_ex():
 
   payload      = 'idddd'
   header       = MSG_UPDATE_PID
+
   proportional = 1.1
   integral     = 2.2
   derivitive   = 3.3
   gain         = 4.0
 
   packer = struct.Struct(payload)
-  pid    = (header,proportional,integral,derivitive,gain)
+  packet = (header,proportional,integral,derivitive,gain)
 
-  tx = packer.pack(*pid)
+  tx = packer.pack(*packet)
   rx = struct.unpack(payload, tx) 
 
-  print("Packed TX packet   : ",tx)
-  print("Unpacked RX packet : ",rx) 
+
+  _header,_proportional,_integral,_derivitive,_gain = rx
+
+  print("Struct Packed TX packet   : ",tx)
+  print("Struct Unpacked RX packet : ",rx) 
+  print("Header                    : ",hex(_header)) 
+  print("Proportional              : ",_proportional) 
+  print("Integral                  : ",_integral) 
+  print("Derivitive                : ",_derivitive) 
+  print("Gain                      : ",_gain) 
+
 
 #***************************************************************************************************************
 
 struct_ex()
+print()
+msgpack_ex()
+
