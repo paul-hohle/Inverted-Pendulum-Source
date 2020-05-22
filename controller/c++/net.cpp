0a7f50e2f7c084d2c888a524dc10a1ea1b6edab5
paul-hohle <hohle.paul@gmail.com>
2020-05-22 15:17:36 +0000

    Network code stub

---
+++ controller/c++/net.cpp
@@ -0,0 +1,19 @@
+//*****************************************************************************************
+
+#include <iostream>
+#include <sstream>
+#include <cassert>
+
+//*****************************************************************************************
+
+#include <array>
+#include <tuple>
+#include <unordered_map>
+#include <unordered_set>
+#include <forward_list>
+#include <string>
+
+//*****************************************************************************************
+
+#include "net.hpp"
+
