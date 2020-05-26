2becf33d17500f91fdd660800d5f7db5621aacac
da82bc6f4bd07a12b24f59a324dcc5b65896a53e <hohle.paul@gmail.com>
2020-05-26 10:15:38 -0700

    Add nng  test code

--- controller/c++/controller.cpp
+++ controller/c++/controller.cpp
@@ -3,6 +3,7 @@
 #include <iostream>
 #include <sstream>
 #include <cassert>
+#include <cstdio>
 
 //*****************************************************************************************
 
@@ -18,6 +19,9 @@
 #include <msgpack.hpp>
 #include <nngpp/nngpp.h>
 #include <robotcontrol.h>
+
+#include <nngpp/protocol/req0.h>
+#include <nngpp/protocol/rep0.h>
 
 //*****************************************************************************************
 
@@ -39,8 +43,59 @@ void packet()
 
 //*****************************************************************************************
 
+int nng_test() try {
+	// create a socket for the rep protocol
+	nng::socket rep_sock = nng::rep::open();
+	
+	// rep starts listening using the tcp transport
+	rep_sock.listen( "tcp://localhost:6666" );
+	
+	// create a socket for the req protocol
+	nng::socket req_sock = nng::req::open();
+	
+	// req dials and establishes a connection
+	req_sock.dial( "tcp://localhost:6666" );
+	
+	// req sends "hello" including the null terminator
+	req_sock.send("hello");
+	
+	// rep receives a message
+	nng::buffer rep_buf = rep_sock.recv();
+	
+	// check the content
+	if( rep_buf == "hello" ) {
+		// rep sends "world" in response
+		rep_sock.send("world");
+               std::cout << "Send world in response" << std::endl;
+	}
+	
+	// req receives "world"
+	nng::buffer req_buf = req_sock.recv();
+
+        return(0);
+}
+
+//----------------------------------------------------------------------------------
+
+catch( const nng::exception& e ) {
+	// who() is the name of the nng function that produced the error
+	// what() is a description of the error code
+	printf( "%s: %s\n", e.who(), e.what() );
+	return 1;
+}
+// req_buf is freed
+// rep_buf is freed
+// req_sock is closed
+// rep_sock is closed
+
+//**********************************************************************************
+
+
 int main(void)
 
 {
-    packet();
+  nng_test();
+  return(0);
 }
+
+
